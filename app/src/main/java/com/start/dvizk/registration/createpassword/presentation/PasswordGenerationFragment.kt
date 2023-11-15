c1bbe1e567e1e61da19d17111147b47898d15d12
Kali Kauymbayev <mr.kauymbaev@mail.ru>
2023-11-16 00:08:13 +0600

    fix return button, toolbar, use viewbinding

--- app/src/main/java/com/start/dvizk/registration/createpassword/presentation/PasswordGenerationFragment.kt
+++ app/src/main/java/com/start/dvizk/registration/createpassword/presentation/PasswordGenerationFragment.kt
@@ -11,8 +11,10 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.start.dvizk.R
+import com.start.dvizk.databinding.FragmentPasswordGenerationBinding
 import com.start.dvizk.registration.registr.presentation.model.User
 import com.start.dvizk.registration.varification.presentation.VerificationCodeFragment
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -22,118 +24,109 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.File
 
 class PasswordGenerationFragment :
-	Fragment(),
-	OnClickListener {
+    Fragment(R.layout.fragment_password_generation),
+    OnClickListener {
 
-	private val passwordGenerationViewModel: PasswordGenerationViewModel by viewModel()
+    private val passwordGenerationViewModel: PasswordGenerationViewModel by viewModel()
+    private val binding: FragmentPasswordGenerationBinding by viewBinding()
 
+    private lateinit var fragment_registration_loader: View
 
-	private lateinit var continueRegistration: Button
-	private lateinit var fragment_password_generation_edit_text_1: TextInputEditText
-	private lateinit var fragment_password_generation_edit_text_2: EditText
-	private lateinit var fragment_registration_loader: View
+    private var email: String = ""
 
-	private var email: String = ""
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? = inflater.inflate(R.layout.fragment_password_generation, container, false)
+        initObservers()
+        fragment_registration_loader = requireActivity().findViewById(R.id.progress_bar)
+        binding.fragmentPasswordGenerationContinueButton.setOnClickListener(this)
+        binding.fragmentPasswordGenerationReturnButton.setNavigationOnClickListener { activity?.onBackPressed() }
+    }
 
+    override fun onClick(view: View?): Unit = with(binding) {
+        when (view?.id) {
+			fragmentPasswordGenerationContinueButton.id -> {
+                arguments?.apply {
+                    val user: User? = getParcelable("user_regis")
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
+                    if (user == null) {
+                        Toast.makeText(
+                            requireContext(),
+                            "Данные заполнены некорректно",
+                            Toast.LENGTH_LONG
+                        ).show()
 
-		initObservers()
-		fragment_password_generation_edit_text_1 =
-			view.findViewById(R.id.fragment_password_generation_edit_text_1)
-		fragment_password_generation_edit_text_2 =
-			view.findViewById(R.id.fragment_password_generation_edit_text_2)
-		fragment_registration_loader = requireActivity().findViewById(R.id.progress_bar)
-		continueRegistration = view.findViewById(R.id.fragment_password_generation_continue_button)
-		continueRegistration.setOnClickListener(this)
-	}
+                        return@apply
+                    }
 
-	override fun onClick(view: View?) {
-		when (view?.id) {
-			continueRegistration.id -> {
-				arguments?.apply {
-					val user: User? = getParcelable("user_regis")
+                    email = user.email
 
-					if (user == null) {
-						Toast.makeText(
-							requireContext(),
-							"Данные заполнены некорректно",
-							Toast.LENGTH_LONG
-						).show()
+                    if (fragmentPasswordGenerationEditText1.text.toString()
+                        == fragmentPasswordGenerationEditText1.text.toString()
+                    ) {
+                        passwordGenerationViewModel.register(
+                            email = user.email,
+                            password = fragmentPasswordGenerationEditText2.text.toString(),
+                            name = user.name,
+                            nickname = user.nickname,
+                            phone_number = user.phone_number,
+                            gender = user.gender,
+                            birthday = user.birthday,
+                            image = getMultipart(user.image),
+                        )
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "Пароли не совпадают",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
 
-						return@apply
-					}
+    private fun initObservers() {
+        passwordGenerationViewModel.passwordGenerationStateLiveData.observe(
+            viewLifecycleOwner,
+            ::handleState
+        )
+    }
 
-					email = user.email
+    private fun handleState(state: PasswordGenerationState) {
+        when (state) {
+            is PasswordGenerationState.Failed -> {
+                fragment_registration_loader.visibility = View.GONE
+                Toast.makeText(requireContext(), state.message, Toast.LENGTH_LONG).show()
+            }
+            is PasswordGenerationState.Loading -> {
+                fragment_registration_loader.visibility = View.VISIBLE
+            }
+            is PasswordGenerationState.Success -> {
+                fragment_registration_loader.visibility = View.GONE
+                val bundle = Bundle().apply {
+                    putString("email", email)
+                }
+                val ft: FragmentTransaction =
+                    requireActivity().supportFragmentManager.beginTransaction()
+                val fragment = VerificationCodeFragment()
+                fragment.arguments = bundle
+                ft.add(R.id.nav_host_fragment_activity_main, fragment)
+                ft.addToBackStack(null)
+                ft.commit()
+            }
+        }
+    }
 
-					if (fragment_password_generation_edit_text_2.text.toString()
-						== fragment_password_generation_edit_text_1.text.toString()) {
-						passwordGenerationViewModel.register(
-							email = user.email,
-							password = fragment_password_generation_edit_text_2.text.toString(),
-							name = user.name,
-							nickname = user.nickname,
-							phone_number = user.phone_number,
-							gender = user.gender,
-							birthday = user.birthday,
-							image = getMultipart(user.image),
-						)
-					} else {
-						Toast.makeText(
-							requireContext(),
-							"Пароли не совпадают",
-							Toast.LENGTH_LONG
-						).show()
-					}
-				}
-			}
-		}
-	}
+    private fun getMultipart(filePath: String?): MultipartBody.Part? {
+        if (filePath.isNullOrEmpty()) {
 
-	private fun initObservers() {
-		passwordGenerationViewModel.passwordGenerationStateLiveData.observe(viewLifecycleOwner, ::handleState)
-	}
+            return null
+        }
+        val file = File(filePath)
 
-	private fun handleState(state: PasswordGenerationState) {
-		when (state) {
-			is PasswordGenerationState.Failed -> {
-				fragment_registration_loader.visibility = View.GONE
-				Toast.makeText(requireContext(), state.message, Toast.LENGTH_LONG).show()
-			}
-			is PasswordGenerationState.Loading -> {
-				fragment_registration_loader.visibility = View.VISIBLE
-			}
-			is PasswordGenerationState.Success -> {
-				fragment_registration_loader.visibility = View.GONE
-				val bundle = Bundle().apply {
-					putString("email", email)
-				}
-				val ft: FragmentTransaction =
-					requireActivity().supportFragmentManager.beginTransaction()
-				val fragment = VerificationCodeFragment()
-				fragment.arguments = bundle
-				ft.add(R.id.nav_host_fragment_activity_main, fragment)
-				ft.addToBackStack(null)
-				ft.commit()
-			}
-		}
-	}
+        val body = RequestBody.create("image/*".toMediaTypeOrNull(), file)
 
-	private fun getMultipart(filePath: String?): MultipartBody.Part? {
-		if (filePath.isNullOrEmpty()) {
-
-			return null
-		}
-		val file = File(filePath)
-
-		val body = RequestBody.create("image/*".toMediaTypeOrNull(), file)
-
-		return MultipartBody.Part.createFormData("image", file.name, body)
-	}
+        return MultipartBody.Part.createFormData("image", file.name, body)
+    }
 }
--- app/src/main/java/com/start/dvizk/registration/registr/presentation/RegistrationFragment.kt
+++ app/src/main/java/com/start/dvizk/registration/registr/presentation/RegistrationFragment.kt
@@ -21,9 +21,11 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.start.dvizk.R
+import com.start.dvizk.databinding.FragmentRegistrationBinding
 import com.start.dvizk.registration.createpassword.presentation.PasswordGenerationFragment
 import com.start.dvizk.registration.dialog.GenderSelectionDialog
 import com.start.dvizk.registration.dialog.GenderSelectionListener
@@ -36,58 +38,29 @@ private const val PICK_IMAGE_REQUEST = 1
 private const val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123
 
 class RegistrationFragment :
-	Fragment(),
+	Fragment(R.layout.fragment_registration),
 	OnClickListener,
 	GenderSelectionListener {
 
 	private val registrationViewModel: RegistrationViewModel by viewModel<RegistrationViewModel>()
+	private val binding: FragmentRegistrationBinding by viewBinding()
 
-	private lateinit var genderSelectionText: TextView
-	private lateinit var returnPage: ImageView
-	private lateinit var profileImageLoader: ImageView
-	private lateinit var dateOfBirth: TextView
-	private lateinit var continueRegistrtion: Button
 	private lateinit var fragment_registration_loader: View
-	private lateinit var fragment_registration_user_name_edit_text: EditText
-	private lateinit var fragment_registration_user_nickname_edit_text: EditText
-	private lateinit var fragment_registration_user_email_edit_text: EditText
-	private lateinit var fragment_registration_user_phone_edit_text: EditText
 
 	var filePath = ""
 	var birthday = ""
 	var gender = ""
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-
-		return inflater.inflate(R.layout.fragment_registration, container, false)
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
 		super.onViewCreated(view, savedInstanceState)
 
 		initObservers()
-		fragment_registration_user_name_edit_text =
-			view.findViewById(R.id.fragment_registration_user_name_edit_text)
-		fragment_registration_user_nickname_edit_text =
-			view.findViewById(R.id.fragment_registration_user_nickname_edit_text)
-		fragment_registration_user_email_edit_text =
-			view.findViewById(R.id.fragment_registration_user_email_edit_text)
-		fragment_registration_user_phone_edit_text =
-			view.findViewById(R.id.fragment_registration_user_phone_edit_text)
-		genderSelectionText = view.findViewById(R.id.fragment_registration_user_gender_spinner)
 		fragment_registration_loader = requireActivity().findViewById(R.id.progress_bar)
-		returnPage = view.findViewById(R.id.fragment_registration_return_button)
-		dateOfBirth = view.findViewById(R.id.fragment_registration_user_birthday_text_view)
-		continueRegistrtion = view.findViewById(R.id.fragment_registration_continue)
-		profileImageLoader = view.findViewById(R.id.fragment_registration_user_image)
-		returnPage.setOnClickListener(this)
-		profileImageLoader.setOnClickListener(this)
-		genderSelectionText.setOnClickListener(this)
-		dateOfBirth.setOnClickListener(this)
-		continueRegistrtion.setOnClickListener(this)
+		fragmentRegistrationReturnButton.setNavigationOnClickListener { activity?.onBackPressed() }
+		fragmentRegistrationUserImage.setOnClickListener(this@RegistrationFragment)
+		fragmentRegistrationUserGenderSpinner.setOnClickListener(this@RegistrationFragment)
+		fragmentRegistrationUserBirthdayTextView.setOnClickListener(this@RegistrationFragment)
+		fragmentRegistrationContinue.setOnClickListener(this@RegistrationFragment)
 	}
 
 	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -115,7 +88,7 @@ class RegistrationFragment :
 				.asBitmap()
 				.load(selectedImageUri)
 				.apply(RequestOptions.circleCropTransform())
-				.into(profileImageLoader)
+				.into(binding.fragmentRegistrationUserImage)
 
 			cursor?.close()
 		}
@@ -134,26 +107,26 @@ class RegistrationFragment :
 		}
 	}
 
-	override fun onClick(view: View?) {
+	override fun onClick(view: View?): Unit = with(binding) {
 		when (view?.id) {
-			genderSelectionText.id -> {
+			fragmentRegistrationUserGenderSpinner.id -> {
 				val dialog = GenderSelectionDialog()
-				dialog.setListener(this)
+				dialog.setListener(this@RegistrationFragment)
 				dialog.show(requireActivity().supportFragmentManager, "GenderSelectionDialog")
 			}
-			returnPage.id -> {
-				requireActivity().supportFragmentManager.popBackStack()
-			}
-			dateOfBirth.id -> {
+//			fragmentRegistrationReturnButton.id -> {
+//				requireActivity().supportFragmentManager.popBackStack()
+//			}
+			fragmentRegistrationUserBirthdayTextView.id -> {
 				getBirthday()
 			}
-			continueRegistrtion.id -> {
+			fragmentRegistrationContinue.id -> {
 				val user = User(
-					email = fragment_registration_user_email_edit_text.text.toString(),
+					email = fragmentRegistrationUserEmailEditText.text.toString(),
 					password = null,
-					name = fragment_registration_user_name_edit_text.text.toString(),
-					nickname = fragment_registration_user_nickname_edit_text.text.toString(),
-					phone_number = fragment_registration_user_phone_edit_text.text.toString(),
+					name = fragmentRegistrationUserNameEditText.text.toString(),
+					nickname = fragmentRegistrationUserNicknameEditText.text.toString(),
+					phone_number = fragmentRegistrationUserPhoneEditText.text.toString(),
 					gender = gender,
 					birthday = birthday,
 					image = filePath,
@@ -171,7 +144,7 @@ class RegistrationFragment :
 				ft.addToBackStack(null)
 				ft.commit()
 			}
-			profileImageLoader.id -> {
+			fragmentRegistrationUserImage.id -> {
 				if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 					ActivityCompat.requestPermissions(requireActivity(),
 						arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -188,11 +161,10 @@ class RegistrationFragment :
 
 	override fun getGender(gender: String) {
 		if (gender == "male") {
-			genderSelectionText.text = "Мужчина"
+			binding.fragmentRegistrationUserGenderSpinner.text = "Мужчина"
 		} else {
-			genderSelectionText.text = "Женщина"
+			binding.fragmentRegistrationUserGenderSpinner.text = "Женщина"
 		}
-
 
 		this.gender = gender
 	}
@@ -213,7 +185,7 @@ class RegistrationFragment :
 			is RegistrationState.Success -> {
 				fragment_registration_loader.visibility = View.GONE
 				val bundle = Bundle().apply {
-					putString("email", fragment_registration_user_email_edit_text.text.toString())
+					putString("email", binding.fragmentRegistrationUserEmailEditText.text.toString())
 				}
 				val ft: FragmentTransaction =
 					requireActivity().supportFragmentManager.beginTransaction()
@@ -235,7 +207,7 @@ class RegistrationFragment :
 		val datePickerDialog = DatePickerDialog(
 			requireContext(),
 			{ view, year, month, dayOfMonth ->
-				dateOfBirth.text = "$year-${month + 1}-$dayOfMonth"
+				binding.fragmentRegistrationUserBirthdayTextView.text = "$year-${month + 1}-$dayOfMonth"
 				birthday = "$year-${month + 1}-$dayOfMonth"
 			}, year, month, dayOfMonth
 		)
