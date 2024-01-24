85b1b430c304f250b2a739f2d0f8f3ecf759234c
Kali Kauymbayev <mr.kauymbaev@mail.ru>
2024-01-24 16:28:50 +0600

    added notification manager

--- app/src/main/java/com/start/eventgo/main/MainActivity.kt
+++ app/src/main/java/com/start/eventgo/main/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.start.eventgo.main
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.start.eventgo.R
 import com.start.eventgo.arch.data.SharedPreferencesRepository
 import com.start.eventgo.auth.main.MainAuthFragment
@@ -83,4 +91,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
+
+//    private fun showSettingDialog() {
+//        MaterialAlertDialogBuilder(
+//            this,
+//            com.google.android.material.R.style.MaterialAlertDialog_Material3
+//        )
+//            .setTitle("Разрешение на уведомление")
+//            .setMessage("Требуется разрешение на уведомление, пожалуйста, разрешите разрешение на уведомление в настройках")
+//            .setPositiveButton("ОК") { _, _ ->
+//                val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+//                intent.data = Uri.parse("package:$packageName")
+//                startActivity(intent)
+//            }
+//            .setNegativeButton("Отменить", null)
+//            .show()
+//    }
+//
+//    private fun showNotificationPermissionRationale() {
+//        MaterialAlertDialogBuilder(
+//            this,
+//            com.google.android.material.R.style.MaterialAlertDialog_Material3
+//        )
+//            .setTitle("Тревога")
+//            .setMessage("Для отображения уведомления требуется разрешение на уведомление")
+//            .setPositiveButton("ОК") { _, _ ->
+//                if (Build.VERSION.SDK_INT >= 33) {
+//                    notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+//                }
+//            }
+//            .setNegativeButton("Отменить", null)
+//            .show()
+//    }
+//
+//    var hasNotificationPermissionGranted = false
 }
---
+++ app/src/main/java/com/start/eventgo/util/MyFirebaseMessagingService.kt
@@ -0,0 +1,96 @@
+package com.start.eventgo.util
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.start.eventgo.R
+import com.start.eventgo.main.SplashScreenActivity
+
+const val channelId = "notification_channel"
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        if (message.notification != null) {
+            if (Build.VERSION.SDK_INT >= 33) {
+                notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+            generateNotification(message.notification!!.title!!, message.notification!!.body!!)
+        }
+    }
+
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                if (Build.VERSION.SDK_INT >= 33) {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                        showNotificationPermissionRationale()
+                    } else {
+                        showSettingDialog()
+                    }
+                }
+            }
+        }
+
+    @SuppressLint("RemoteViewLayout")
+    fun getRemoteView(title: String, message: String): RemoteViews {
+        val remoteView = RemoteViews("com.start.eventgo.util", R.layout.notification)
+
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.message, title)
+        remoteView.setTextViewText(R.id.title, title)
+
+        return remoteView
+    }
+
+    private fun generateNotification(title: String, message: String) {
+        val intent = Intent(this, SplashScreenActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val intentFlags: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_ONE_SHOT
+        } else {
+            PendingIntent.FLAG_ONE_SHOT
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, intentFlags)
+
+        var builder: NotificationCompat.Builder =
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.logo_event)
+                .setAutoCancel(true)
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
+                .setOnlyAlertOnce(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title, message))
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val description = "Notifications for download status"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_HIGH)
+            mChannel.description = description
+            mChannel.setShowBadge(true)
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.BLUE
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        notificationManager.notify(1, builder.build())
+    }
+}
